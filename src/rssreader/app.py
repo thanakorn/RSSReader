5661b35998b6aea76fb58df78de17af1cd5520b6
Thanakorn <stylus_l@hotmail.com>
2013-01-08 19:45:36 +0700

    Add file app.py

--- src/rssreader/app.py
+++ src/rssreader/app.py
@@ -3,3 +3,11 @@ Created on 8/1/2013
 
 @author: Thanakorn
 '''
+import sys
+
+def main():
+    print "OK"  # give some feed back
+    return 0    # exit code
+
+if __name__ == "__main__":
+    sys.exit(main())
